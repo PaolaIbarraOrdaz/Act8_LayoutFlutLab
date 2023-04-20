ef7bf8a4298da2354bd4ee09bca31f10747beee9
PAOLA_IBARRA_ORDAZ <a.20308051280514@cbtis128.edu.mx>
2023-04-20 16:45:37 +0000

    vCorrecta

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,128 @@
 import 'package:flutter/material.dart';
+// Uncomment lines 3 and 6 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Dulcería Alegrías',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Ciudad Juárez, Chihuahua',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          const Icon(
+            Icons.star,
+            color: Color(0xffa70e03),
+          ),
+          const Text('41'),
+        ],
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+    Color color = const Color(0xffb80303);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'Teléfono'),
+        _buildButtonColumn(color, Icons.near_me, 'Ubicación'),
+        _buildButtonColumn(color, Icons.share, 'Compartir'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'La mejor tienda de dulces mexicanos en Cd Juárez, Chihuahua. '
+        'Encontrarás los mejores productos de excelente calidad, te '
+        'encantará la gran variedad de dulces que tenemos para ti. Aprovecha '
+        'los nuevos paquetes de fiesta que tenemos para tus celebraciones. '
+        'Nuestra misión es lo que nos impulsa a hacer todo lo posible '
+        'para seguir con los dulces tradicionales, creando productos de '
+        'excelente calidad. Queremos que los dulces típicos mexicanos '
+        'no sean olvidados, ya que forman parte de nuestras tradiciones, de nuestra historia.',
+        softWrap: true,
+        textAlign: TextAlign.justify,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+
+    return MaterialApp(
+      title: 'Dulcería Alegrías',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Dulcería Alegrías'),
+          backgroundColor: const Color(0xffb80303),
+          actions: const [Icon(Icons.search), Icon(Icons.more_vert)],
+          elevation: 8,
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/fondo_logo.png',
+              width: 600,
+              height: 360,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
